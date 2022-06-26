28bf46d0ac97d93fe9d39f076d4e9731ea11d721
Karabo Mashao <karabomashao1@gmail.com>
2022-06-26 09:54:28 -0700

    c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - This is a short description
- * return: The value of void
- */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
