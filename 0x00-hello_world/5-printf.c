9ee7109e4dc6acd13cfcb04578bbb456ad59dbf5
Karabo Mashao <karabomashao1@gmail.com>
2022-06-26 10:33:51 -0700

    Print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if true, else non-zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);
+}
