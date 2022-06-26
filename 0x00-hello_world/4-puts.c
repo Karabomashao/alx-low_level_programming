f876e23556c28e152158d378cb750a6ab9c4c7ac
Karabo Mashao <karabomashao1@gmail.com>
2022-06-26 10:02:17 -0700

    My working c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - This is a short description
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
