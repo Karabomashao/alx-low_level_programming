723495a756da0187313ab6a2c4a1640ddcf88c39
Karabo Mashao <karabomashao1@gmail.com>
2022-06-30 13:16:47 -0700

    Using putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
