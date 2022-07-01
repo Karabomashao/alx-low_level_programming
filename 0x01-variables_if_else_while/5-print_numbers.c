bd048cb1276003d4758531fde3f3001d405c0706
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 11:12:56 -0700

    Basee 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print base 10 numerals from 0 - 9
+ * Return - True if 0 false otherwise
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf(n"\n");
+	}
+	return (0);
+}
