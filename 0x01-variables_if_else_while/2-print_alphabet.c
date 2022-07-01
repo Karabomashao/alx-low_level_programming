ead3ff4ddab7d633589c6052b87b9881e038b153
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 08:53:43 -0700

    Puthcar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - use a putchar function to display a-z alphaberts
+ *
+ */
+int main()
+{
+	int chr;
+	for (chr = "a"; chr <= "z"; chr++)
+		putchar(ch);
+	return 0;
+}
