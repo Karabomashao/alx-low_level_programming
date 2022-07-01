4429a229459a0af8cad1b21d537d6502ba887117
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 11:27:36 -0700

    Base 10 numerals using puthar)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - returns base 10 digits
+ * Retun - True if 0 false otherwise
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig <= 9; dig++)
+	{
+		putchar(dig);
+	}
+	putchar('\n');
+	return (0);
+}
