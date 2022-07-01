85fb356fb8589310db2ebc9136d6589858a470a3
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 13:16:24 -0700

    single digit combos

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - putchar to list 1,9
+ * Return: true if 0 false otherwise
+ */
+int main(void)
+{
+    int x;
+
+    for (x = 0; x < 10; x++)
+    {
+        putchar(x + '0');
+        if (x < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+    return (0);
+}
