090153efdd4630ffd9221637a836f6163dafe1b1
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 12:05:07 -0700

    Reverse alpharbets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - > for the programm
+ * Return - 0 if true 
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
