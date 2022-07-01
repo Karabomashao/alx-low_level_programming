62181149bc85ee284205b067886420d5e02ad5ca
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 13:03:18 -0700

    Base 16 - hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all hex characters
+ * Return: successful = 0
+ */
+int main(void)
+{
+	char n, ch;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchat(n);
+	}
+	for (ch = 'A'; ch <= 'F'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}	
