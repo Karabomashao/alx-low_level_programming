e370bade8d5bfe0aecebd2e7690812f7bd8a1ed4
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 15:03:19 -0700

    Combinations of two

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n<=9; n++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+			if (n != m && m != n)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
