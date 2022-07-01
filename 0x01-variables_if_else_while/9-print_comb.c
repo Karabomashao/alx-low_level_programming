645809ae7f877d7d0e9757e617acdbd82ed49f28
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 13:21:45 -0700

    1,-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - putchar to list 1,9
+ * main - putchar to list 1
  * Return: true if 0 false otherwise
  */
 int main(void)
 {
-    int x;
+	int x;
 
-    for (x = 0; x < 10; x++)
-    {
-        putchar(x + '0');
-        if (x < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+    	for (x = 0; x < 10; x++)
+    	{
+        	putchar(x + '0');
+        	if (x < 9)
+        	{
+            		putchar(',');
+            		putchar(' ');
+        	}
+    	}
+    	putchar('\n');
+    	return (0);
 }
