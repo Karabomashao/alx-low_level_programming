371571a0e25a4f900d973b0bc52a0cbc371d0787
Karabo Mashao <karabomashao1@gmail.com>
2022-07-01 13:25:37 -0700

    1,-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int x;
 
-    	for (x = 0; x < 10; x++)
-    	{
-        	putchar(x + '0');
-        	if (x < 9)
-        	{
-            		putchar(',');
-            		putchar(' ');
-        	}
-    	}
-    	putchar('\n');
-    	return (0);
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
