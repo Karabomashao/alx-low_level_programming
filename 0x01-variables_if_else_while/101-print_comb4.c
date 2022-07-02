50dc98346110bb602cbea1f3fdf10952af4d3584
Karabo Mashao <karabomashao1@gmail.com>
2022-07-02 02:32:24 -0700

    Combination of 3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = 1; b <= 8; b++)
+		{
+			for (c = 2; c <= 9; c++)
+			{
+				if (b > a && b < c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+				}
+				if (a < b && a < c && a != 7)
+				{
+					putchar(',')
+					putchar(' ')
+				}
+			}
+		}
+	}
+}
+
