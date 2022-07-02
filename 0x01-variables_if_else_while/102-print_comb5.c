bfdac99ff0ddcb1a06a387d1217b198adce5c411
Karabo Mashao <karabomashao1@gmail.com>
2022-07-02 05:48:29 -0700

    double double

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 8; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 1; d <= 9; d++)
+				{
+					if ( d > b)
+					{
+						putchar(a);
+						putchar(b);
+						purchar(' ');
+						putchar(c);
+						putchar(d);
+					}
+					if (b < d)
+					{
+						putchar(',')
+						putchar(' ')
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
