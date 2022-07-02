a6e7550492f435f46c81e97ddff28bdb8d733cb3
Karabo Mashao <karabomashao1@gmail.com>
2022-07-02 02:02:05 -0700

    two combos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,15 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 0; n<=9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		for (m = 1; m <= 9; m++)
 		{
 			if (m > n)
-				break;
-			putchar(n + '0');
-			putchar(m + '0');
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+			}
 			if (n != 8 || m != 9)
 			{
 				putchar(',');
