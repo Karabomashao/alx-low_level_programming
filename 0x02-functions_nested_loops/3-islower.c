b7cd01356f8d7ce12018ac312b68557e2213c08d
Karabo Mashao <karabomashao1@gmail.com>
2022-07-05 12:48:13 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks lowercase characters
+ * @c: a character argument
+ * Return: prints 0 or one depending on the conditiond
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
