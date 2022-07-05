2e622c900f88f100d8141fd9e100755dc999b618
Karabo Mashao <karabomashao1@gmail.com>
2022-07-04 23:31:46 -0700

    10 times alpheberts

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int i;
+	char a;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
