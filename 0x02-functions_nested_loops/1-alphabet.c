ba2880227603daf8807c893d7aee70f9cce52f05
Karabo Mashao <karabomashao1@gmail.com>
2022-07-05 11:34:04 -0700

    Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - print alphabet in lower cases
  * Return: Always 0 (succesful)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 97; i <= 122; i++)
 	{
 		putchar(i);
-	g
+	}
 	putchar('\n');
 	return (0);
 }
