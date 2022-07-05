1e26eeeb3e68d791833c33a82b841c444ea4281a
Karabo Mashao <karabomashao1@gmail.com>
2022-07-05 14:59:04 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 			{
 				for (l = 0; l <= 9; l++)
 				{
-					if (i < 2 && j < 4) 
+					if (i <= 2 && j < 4 && j <= 5)
 					{
 						_putchar(i + '0');
 						_putchar(j + '0');
