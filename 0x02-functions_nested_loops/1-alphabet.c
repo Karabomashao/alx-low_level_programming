01abe5aaa7f55c5bf21ec175811118fd7df1196e
Karabo Mashao <karabomashao1@gmail.com>
2022-07-04 23:20:27 -0700

    Alphaberts

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (succesful)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n')
+	return: Always 0 (successful)
+}
