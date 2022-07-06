af5399768cdb902f3d05041f6a265a7f331519e9
Karabo Mashao <karabomashao1@gmail.com>
2022-07-06 03:02:17 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,121 +6,32 @@
  */
 void times_table(void)
 {
-	int  a, b, c, d, e;
-	int i, j, k, l, m, n;
-
-	for (i = 1; i <= 10; i++)
+	int  row, column, multiplier;
+	
+	for (row = 0; row <= 9; row++)
 	{
-		for (a = 0; a <= 9; a++)
+		_putchar(0 + '0');
+		_putchar(',');
+		_putchar(' ');
+		for (column = 1; column <= 9; column++)
 		{
-			if ( a <= 0)
+			multiplier = row * column;
+			if ((multiplier / 10) > 0)
 			{
-				_putchar(a);
+				_putchar((multiplier / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((multiplier % 10) + '0');
+			if (column < 9)
+			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar('\n');
-			}
-			for (b = 0; b <= 9; b++)
-			{
-				if ( b >= 0 && b < 9)
-				{
-					_putchar(b);
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('\n');
-				}
-				for (c = 0; c <= 9; c+=2)
-				{
-					if (c >= 0 && c < 18)
-					{
-						_putchar(b);
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('\n');
-					}
-					for (d = 0; d <= 9; d+=3)
-					{
-						if (d >= 0 && d < 27)
-						{
-							 _putchar(d);
-							 _putchar(',');
-							 _putchar(' ');
-							 _putchar(' ');
-							 _putchar('\n');
-						}
-						for (e = 0; e <= 9; e+=4)
-						{
-							if (e >= 0 && e < 36)
-							{
-								_putchar(e);
-								_putchar(',');
-								_putchar(' ');
-								_putchar(' ');
-								_putchar('\n');
-							}
-							for (j = 0; j <= 9; j+=5)
-							{
-								if (j >= 0 && j < 45)
-								{
-									_putchar(j);
-									_putchar(',');
-									_putchar(' ');
-									_putchar(' ');
-									_putchar('\n');
-								}
-								for (k = 0; k <= 9; k+=6)
-								{
-									if (k >= 0 && k < 54)
-									{
-										_putchar(k);
-										_putchar(',');
-										_putchar(' ');
-										_putchar(' ');
-										_putchar('\n');
-									}
-									for (l = 0; l <= 9; l+=7)
-									{
-										if (l >= 0 && l < 63)
-										{
-											_putchar(l);
-											_putchar(',');
-											_putchar(' ');
-											_putchar(' ');
-											_putchar('\n');
-										}
-										for (m = 0; m <= 9; m+=8)
-										{
-											if (m >= 0 && m < 72)
-											{
-												_putchar(b);
-												_putchar(',');
-												_putchar(' ');
-												_putchar(' ');
-												_putchar('\n');
-											}
-											for (n = 0; n <= 9; n++)
-											{
-												if (n >= 0 && n < 81)
-												{
-													_putchar(b);
-													_putchar(',');
-													_putchar(' ');
-													_putchar(' ');
-													_putchar('\n');
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
 			}
 		}
+		_putchar('\n');
 	}
 }
 
