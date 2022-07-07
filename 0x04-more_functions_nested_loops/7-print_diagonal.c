12ecb41d173652d43f088c3294da1b1a2a364e61
Karabo Mashao <karabomashao1@gmail.com>
2022-07-07 07:07:01 -0700

    Diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line in terminal
+ * @n: parameter
+ */
+void print_diagonal(int n)
+{
+	int column, i;
+	char diagonal;
+
+	diagonal = '\\';
+	for (column = 1; column <= n; column++)
+	{
+		_putchar(diagonal);
+		_putchar('\n');
+		for (i = 1; i <= column; i++)
+		{
+			if (column < n)
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
