1665816db3b641e4793ca008f08f952bb92b5487
Karabo Mashao <karabomashao1@gmail.com>
2022-07-07 12:48:39 -0700

    Traingle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,19 +8,26 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j <= (size + 1) - i; j++)
-		{
-			if (j > 1)
-			{
-			_putchar(' ');
-			}
-		}
-		for (k = 1; k <= i; k++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= (size + 1) - i; j++)
+			{
+				if (j > 1)
+				{
+				_putchar(' ');
+				}
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
 	}
 }
