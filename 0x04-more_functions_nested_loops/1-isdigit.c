3e0c4a318296e0f631aeb7004792bafd326d3391
Karabo Mashao <karabomashao1@gmail.com>
2022-07-07 02:47:56 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits
+ * @c: parameter
+ * Return: 1 or 0 depending on the conidtion
+ */
+int _isdigit(int c)
+{
+	return (c = 0 + '0' && c <= 9 + '0');
+}
