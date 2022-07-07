806c6cfbd3872ba8a083ebcae09024678e8f6d7e
Karabo Mashao <karabomashao1@gmail.com>
2022-07-07 12:41:40 -0700

    Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle made from #
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= (size + 1) - i; j++)
+		{
+			if (j > 1)
+			{
+			_putchar(' ');
+			}
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
