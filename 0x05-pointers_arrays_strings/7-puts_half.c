301f053f959f976748f0dd61ca841eb019e3605d
Karabo Mashao <karabomashao1@gmail.com>
2022-07-11 09:59:04 -0700

    last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - pints last half of string
+ * @:str - parameter
+ */
+void puts_half(char *str)
+{
+	int i, j, n, len_str;
+
+	len_str = strlen(str);
+	n = (len_str - 1) / 2;
+	for (i = n; i <= len_str - 1; i++)
+	{
+		if (n % 2 == 0)
+		{
+			for (j = n + 1; j <= len_str; j++)
+			{
+				_putchar(str[j]);
+			}
+			break;
+		}
+		else
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
