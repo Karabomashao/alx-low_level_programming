7f1f9b5c041766ed22c966ccfb29e2c0556e647c
Karabo Mashao <karabomashao1@gmail.com>
2022-07-18 09:55:22 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - returns frst occurance of letter
+ * @s: parameter s
+ * @c: parameter
+ * Return: string from first occurance of letter
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
