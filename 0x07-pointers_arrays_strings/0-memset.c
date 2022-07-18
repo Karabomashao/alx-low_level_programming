103412920fc8d8a2c72cfbe774f594b3499b2061
Karabo Mashao <karabomashao1@gmail.com>
2022-07-18 02:57:17 -0700

    fill memory blocks withh bytes

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - fill memory with constant byte
+ * @s: pointer to char
+ * @b: parameter b
+ * @i: number of bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (memset(s, b, n));
+}
